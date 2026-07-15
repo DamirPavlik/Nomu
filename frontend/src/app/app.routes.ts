e5440430a6e1678b8ac4c8aa27635955e1aedb3b
DamirPavlik <damir.pavlik@gmail.com>
2026-07-15 20:49:16 +0200

    feat(fe): add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Home } from './features/home/home';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: Login},
+    {path: 'register', component: Register},
+    {path: 'home', component: Home, canActivate: [authGuard]},
+    {path: '', pathMatch: 'full', redirectTo: 'login'},
+];
