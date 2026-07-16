71e54677cdc099ef2f6783607698ab28d4fab4ad
DamirPavlik <damir.pavlik@gmail.com>
2026-07-16 11:54:09 +0200

    feat: add config

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    database_url: str
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+    jwt_access_token_expire_minutes: int = 60
+
+settings = Settings()
